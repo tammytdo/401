2e4696823ac0a204bfd73b45bfc87a266f986533
tammytdo11 <tammytdo11@gmail.com>
2019-07-17 08:57:40 -0700

    building server

--- city_explorer/index.py
+++ city_explorer/index.py
@@ -1,54 +1,61 @@
-# Features
-# In index.py create the following endpoints:
-
-# GET /locations?data=barcelona - returns a valid json response that matches structure { 'formatted_query':'the place', 'search_query':'what they typed', 'latitude':100.0, 'longitude':100.0 }
-# Stretch Goals
-# Make super class to encapsulate common resource functionality
-# Make a static method on resource class to handle fetching the data from api
-
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
 import os
 import json
 import requests
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+# Python package that searches for a .env. If found, will expose the variables in it to the app. 
+# from dotenv import load_dotenv
+# load_dotenv()
+ 
+PORT = 3000
 
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_path = urlparse(self.path)
-
         print('request path', parsed_path.path)
 
         parsed_qs = parse_qs(parsed_path.query)
-
         print('parsed query', parsed_qs)
         
-        if parsed_path.path == '/':
-            print("hello code")
-            self.send_response(200, "string")
-            self.send_header('Content-type', 'text')
-            self.end_headers()
+        if parsed_path.path == '/locations':
+            # self.send_response(200)
+            # self.send_header('Content-type', 'application/json')
+            # self.end_headers()
 
-            json_string = json.dumps("json stringy")
+            query = parsed_qs['search_query'] #the searched item
+            url = f'https://maps.googleapis.com/maps/api/geocode/json?address={query}&key={GOOGLE_API_KEY}'
+            print('The URL is ', url)
+
+            result = requests.get(url).json()
+            
+            self.formatted_query = result['results'][0]['formatted_address']
+            self.latitude = result['results'][0]['geometry']['location']['lat']
+            self.longitude = result['results'][0]['geometry']['location']['lng']
+
+            json_string = json.dumps(result)
             self.wfile.write(json_string.encode())
             return
 
+        self.send_response_only(404)
+        self.end_headers()
 
 def create_server():
     return HTTPServer(
-    ('127.0.0.1', 3000), SimpleHTTPRequestHandler
+    ('127.0.0.1', PORT), SimpleHTTPRequestHandler
 )
 
 def run_forever():
     server = create_server()
 
     try: 
-        print(f'Starting server on port 3000')
+        print(f'Starting server on PORT {PORT}')
         server.serve_forever()
 
     except KeyboardInterrupt:
         server.server_close()
         server.shutdown()
+        print('Ended server.')
 
 if __name__ == "__main__":
     run_forever()
