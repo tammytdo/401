85c580e58e91c7236ab2c97ae5bbed29c96dcbab
tammytdo11 <tammytdo11@gmail.com>
2019-08-13 13:56:05 -0700

    django app with postgres database

--- django/mysite/mysite/settings.py
+++ django/mysite/mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tammydo',
+        'USER': 'tammydo',
+        'HOST': 'localhost', # set in docker-compose.yml
+        'PASSWORD': 'docker', # DANGER get this out of here!
+        'PORT': 5432 # default postgres port
     }
 }
 
